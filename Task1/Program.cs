805aa562a5e408a38bcdb4d5a65b34a75be755b0
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-06-11 15:24:15 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int prompt(string message)
+{
+    Console.Write(message);  
+    string NumberStr = Console.ReadLine();
+    int number = int.Parse(NumberStr);
+    return number;
+}
+int number = prompt("Введите трехзначное число: ");
+System.Console.WriteLine($"{number} -> {number % 100 / 10}");
