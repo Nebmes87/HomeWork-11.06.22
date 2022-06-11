51150e174379b17ebf7ecbf97f1c4fe9de6f940b
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-06-11 15:53:41 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+int numberC = new Random().Next(1, 10);
+System.Console.WriteLine($"{numberA}{numberB}{numberC} -> {numberA}{numberC}");
